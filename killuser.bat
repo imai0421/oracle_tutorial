91ba72eb1638f6b3a872aa131e6a7416b9e3e1a7
imai0421 <82428053+imai0421@users.noreply.github.com>
2021-04-22 17:39:04 +0900

    引数が指定されていない場合はエラーメッセージを表示する#1

--- killuser.bat
+++ killuser.bat
@@ -1,3 +1,12 @@
-echo on
+@echo off
+
+if "%1" == "" goto PARAM_ERROR
+
 SQLPLUS -s SYS/password@XE AS SYSDBA @killuser.sql %1
-exit;
+
+echo %1 のセッションを切断しました。
+exit /B
+
+:PARAM_ERROR
+echo 引数にセッションを破棄したいユーザ名(スキーマ名)を指定してください。
+exit /B
